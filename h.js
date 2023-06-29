3cec833bf444146b19d558f786b3f5ffe8603eeb
JWarconsin <133745642+JWarconsin@users.noreply.github.com>
2023-06-29 18:16:53 +0200

    Third attempt

--- h.js
+++ h.js
@@ -1,5 +1,8 @@
-let list = [];
-
+let kanji_list = [];
+let popup;
+let section_kanji = document.getElementById("kanji")
+popup = document.getElementById("PopUp")
+console.log(popup)
 readKANJIFile("日本語 - ぼＢ.tsv");
 
 
@@ -36,15 +39,44 @@ function createKanji(pFile) {
         exemple: row[i][4],
         Francais:row[i][5]
       };
-      list.push(kanji)
+      kanji_list.push(kanji)
        //list.push(row[i][0]);
        
     }
-    console.log(list);
+    console.log(kanji_list);
 }
 
 function OpenPopUp(id){
-console.log(list[id].kanji)
+    popup.style.display = "block";
+    popup.innerHTML = "";
+    let innerHTML;
+    innerHTML = `
+    <div id = "divkanji"><p>${kanji_list[id].kanji}</p></div>
+    <p class = "popupkanji_p"><span>音読み : </span>${kanji_list[id].onyomi}</p> 
+    <p class = "popupkanji_p"><span>訓読み : </span>${kanji_list[id].kunyomi}</p>
+    <p class = "popupkanji_p"><span>れい : </span>${kanji_list[id].rei}</p> 
+    <p class = "popupkanji_p"><span>例 : </span>${kanji_list[id].exemple}</p> 
+    <p class = "popupkanji_p"><span>フランス語 : </span>${kanji_list[id].Francais}</p> 
+    `;
+popup.innerHTML = innerHTML;
+    // popup.innerHTML = kanji_list[id].kanji
+    console.log(kanji_list[id].kanji);
 
 
+
+}
+function closepopup(){
+    popup.style.display = "none";
+}
+function kanji_search(){
+    section_kanji.innerHTML = "";
+    let innerHTML;
+    innerHTML = `<div class="button_kanji">`
+    for(let i = 0;i < kanji_list.length-1 ; i++){
+        innerHTML += `<button id = "${i}" onclick="OpenPopUp(${i})">${kanji_list[i].kanji}</button>`
+       
+        console.log(kanji_list[i].kanji);
+    }
+    innerHTML += "</div>"
+    section_kanji.innerHTML = innerHTML;
 }
