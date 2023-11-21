b0e57b774c13b097935c37955f4633dec9399c6c
JWarconsin <133745642+JWarconsin@users.noreply.github.com>
2023-11-21 08:28:24 +0100

    21_11_23

--- h.js
+++ h.js
@@ -1,8 +1,17 @@
 let kanji_list = [];
 let popup;
-let section_kanji = document.getElementById("kanji")
-popup = document.getElementById("PopUp")
-console.log(popup)
+let section_kanji = document.getElementById("section_kanji");
+let search_input = document.getElementById("search_input");
+let button_kanji = document.getElementById("button_kanji");
+let bminna = document.getElementById("bminna");
+search_input.addEventListener("keydown",e=>{
+    if (e.code == "Enter"){
+        kanji_search();
+    }
+});
+// e.code="Enter"
+let search_list = [];
+popup = document.getElementById("PopUp");
 readKANJIFile("日本語 - ぼＢ.tsv");
 
 
@@ -22,8 +31,6 @@ function readKANJIFile(pFile) {
     rawFile.send(null);    
 }
 
-
-
 function createKanji(pFile) {
     let row = pFile.split(/\r\n|\n/);
     let test;
@@ -39,44 +46,86 @@ function createKanji(pFile) {
         exemple: row[i][4],
         Francais:row[i][5]
       };
-      kanji_list.push(kanji)
+      kanji_list.push(kanji);
        //list.push(row[i][0]);
        
     }
-    console.log(kanji_list);
+    kanji_search();
 }
 
-function OpenPopUp(id){
+function OpenPopUp(id, liste){
     popup.style.display = "block";
     popup.innerHTML = "";
     let innerHTML;
     innerHTML = `
-    <div id = "divkanji"><p>${kanji_list[id].kanji}</p></div>
-    <p class = "popupkanji_p"><span>音読み : </span>${kanji_list[id].onyomi}</p> 
-    <p class = "popupkanji_p"><span>訓読み : </span>${kanji_list[id].kunyomi}</p>
-    <p class = "popupkanji_p"><span>れい : </span>${kanji_list[id].rei}</p> 
-    <p class = "popupkanji_p"><span>例 : </span>${kanji_list[id].exemple}</p> 
-    <p class = "popupkanji_p"><span>フランス語 : </span>${kanji_list[id].Francais}</p> 
+    <div id = "divkanji"><p>${liste[id].kanji}</p></div>
+    <p class = "popupkanji_p"><span>音読み : </span>${liste[id].onyomi}</p> 
+    <p class = "popupkanji_p"><span>訓読み : </span>${liste[id].kunyomi}</p>
+    <p class = "popupkanji_p"><span>れい : </span>${liste[id].rei}</p> 
+    <p class = "popupkanji_p"><span>例 : </span>${liste[id].exemple}</p> 
+    <p class = "popupkanji_p"><span>フランス語 : </span>${liste[id].Francais}</p>
     `;
-popup.innerHTML = innerHTML;
+    popup.innerHTML = innerHTML;
     // popup.innerHTML = kanji_list[id].kanji
-    console.log(kanji_list[id].kanji);
-
-
 
 }
 function closepopup(){
     popup.style.display = "none";
 }
+
 function kanji_search(){
-    section_kanji.innerHTML = "";
-    let innerHTML;
-    innerHTML = `<div class="button_kanji">`
-    for(let i = 0;i < kanji_list.length-1 ; i++){
-        innerHTML += `<button id = "${i}" onclick="OpenPopUp(${i})">${kanji_list[i].kanji}</button>`
-       
-        console.log(kanji_list[i].kanji);
+    if(search_input.value == "")
+    {
+        button_kanji.innerHTML = "";
+        let innerHTML = "";
+        for(let i = 0;i < kanji_list.length-1 ; i++)
+        {
+            innerHTML += `<button id = "${i}" onclick="OpenPopUp(${i},kanji_list)">${kanji_list[i].kanji}</button>`
+
+        }
+        button_kanji.innerHTML = innerHTML;
     }
-    innerHTML += "</div>"
-    section_kanji.innerHTML = innerHTML;
+    else
+    {
+        search_list = [];
+        for(let i = 0; i < kanji_list.length-1 ; i++)
+        {
+            if(kanji_list[i].kanji.includes(search_input.value))
+            {
+                search_list.push(kanji_list[i]);
+            }
+            else if(kanji_list[i].onyomi.includes(search_input.value))
+            {
+                search_list.push(kanji_list[i]);
+            }
+            else if(kanji_list[i].kunyomi.includes(search_input.value))
+            {
+                search_list.push(kanji_list[i]);
+            }
+            else if(kanji_list[i].Francais.includes(search_input.value))
+            {
+                search_list.push(kanji_list[i]);
+            }
+
+        }
+        button_kanji.innerHTML = "";
+        let innerHTML = "";
+        for(let i = 0;i < search_list.length ; i++)
+        {
+            innerHTML += `<button id = "${i}" onclick="OpenPopUp(${i},search_list)">${search_list[i].kanji}</button>`
+        }
+        button_kanji.innerHTML = innerHTML;
+
+    }
+
+}
+
+function minna(){
+    section_kanji.style.display = "none";
+
+}
+
+function All(){
+    section_kanji.style.display = "flex";
+
 }
